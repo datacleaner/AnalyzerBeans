711c7ff450935a5a94967a6a0cd3e03c5a0c759b
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2010-07-04 19:42:29 +0000

    Improved SingleThreadedTaskRunner to include a queuing mechanism to ensure that tasks are executed in the correct order even with recursive task scheduling.

--- src/main/java/org/eobjects/analyzer/job/concurrent/SingleThreadedTaskRunner.java
+++ src/main/java/org/eobjects/analyzer/job/concurrent/SingleThreadedTaskRunner.java
@@ -1,11 +1,42 @@
 package org.eobjects.analyzer.job.concurrent;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 import org.eobjects.analyzer.job.tasks.Task;
 
 public class SingleThreadedTaskRunner implements TaskRunner {
 
+	private BlockingQueue<Task> _tasks = new LinkedBlockingQueue<Task>();
+	private boolean _queueTasks;
+	private boolean _running = false;
+
+	public SingleThreadedTaskRunner() {
+		this(true);
+	}
+
+	public SingleThreadedTaskRunner(boolean queueTasks) {
+		_queueTasks = queueTasks;
+	}
+
 	@Override
 	public void run(Task task) {
+		if (_queueTasks) {
+			_tasks.add(task);
+			if (!_running) {
+				_running = true;
+				while (!_tasks.isEmpty()) {
+					Task nextTask = _tasks.poll();
+					exec(nextTask);
+				}
+				_running = false;
+			}
+		} else {
+			exec(task);
+		}
+	}
+
+	private void exec(Task task) {
 		try {
 			task.execute();
 		} catch (Exception e) {
---
+++ src/test/java/org/eobjects/analyzer/job/concurrency/SingleThreadedTaskRunnerTest.java
@@ -0,0 +1,60 @@
+package org.eobjects.analyzer.job.concurrency;
+
+import org.eobjects.analyzer.job.concurrent.SingleThreadedTaskRunner;
+import org.eobjects.analyzer.job.concurrent.TaskRunner;
+import org.eobjects.analyzer.job.tasks.Task;
+
+import junit.framework.TestCase;
+
+public class SingleThreadedTaskRunnerTest extends TestCase {
+
+	private static class SimpleRecursiveTask implements Task {
+		private TaskRunner taskRunner;
+		private Task nextTask;
+		private char charToPrint;
+		private StringBuilder sb;
+
+		public SimpleRecursiveTask(StringBuilder sb, char c, Task nextTask,
+				TaskRunner taskRunner) {
+			this.sb = sb;
+			this.charToPrint = c;
+			this.nextTask = nextTask;
+			this.taskRunner = taskRunner;
+		}
+
+		@Override
+		public void execute() throws Exception {
+			sb.append(charToPrint);
+			if (nextTask != null) {
+				taskRunner.run(nextTask);
+			}
+			sb.append(charToPrint);
+		}
+	}
+
+	public void testNonQueuedChronology() throws Exception {
+		SingleThreadedTaskRunner runner = new SingleThreadedTaskRunner(false);
+
+		StringBuilder sb = new StringBuilder();
+		Task task3 = new SimpleRecursiveTask(sb, 'c', null, null);
+		Task task2 = new SimpleRecursiveTask(sb, 'b', task3, runner);
+		Task task1 = new SimpleRecursiveTask(sb, 'a', task2, runner);
+
+		runner.run(task1);
+
+		assertEquals("abccba", sb.toString());
+	}
+
+	public void testQueuedChronology() throws Exception {
+		SingleThreadedTaskRunner runner = new SingleThreadedTaskRunner();
+
+		StringBuilder sb = new StringBuilder();
+		Task task3 = new SimpleRecursiveTask(sb, 'c', null, null);
+		Task task2 = new SimpleRecursiveTask(sb, 'b', task3, runner);
+		Task task1 = new SimpleRecursiveTask(sb, 'a', task2, runner);
+
+		runner.run(task1);
+
+		assertEquals("aabbcc", sb.toString());
+	}
+}
