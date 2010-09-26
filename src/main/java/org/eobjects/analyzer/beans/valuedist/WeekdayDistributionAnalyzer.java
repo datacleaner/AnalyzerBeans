2a8800a49d6630fc05544dff46f65003cf740d1c
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2010-09-26 09:20:20 +0000

    Minor fix for Weekday Distribution analyzer

--- src/main/java/org/eobjects/analyzer/beans/valuedist/WeekdayDistributionAnalyzer.java
+++ src/main/java/org/eobjects/analyzer/beans/valuedist/WeekdayDistributionAnalyzer.java
@@ -17,8 +17,7 @@ import org.eobjects.analyzer.result.CrosstabNavigator;
 import org.eobjects.analyzer.result.CrosstabResult;
 
 @AnalyzerBean("Weekday distribution")
-public class WeekdayDistributionAnalyzer implements
-		RowProcessingAnalyzer<CrosstabResult> {
+public class WeekdayDistributionAnalyzer implements RowProcessingAnalyzer<CrosstabResult> {
 
 	private Map<InputColumn<Date>, Map<Integer, Integer>> distributionMap;
 
@@ -29,7 +28,7 @@ public class WeekdayDistributionAnalyzer implements
 	public void init() {
 		distributionMap = new HashMap<InputColumn<Date>, Map<Integer, Integer>>();
 		for (InputColumn<Date> col : dateColumns) {
-			Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+			Map<Integer, Integer> countMap = new HashMap<Integer, Integer>(7);
 			for (int i = Calendar.SUNDAY; i <= Calendar.SATURDAY; i++) {
 				// put a count of 0 for each day of the week
 				countMap.put(i, 0);
@@ -58,35 +57,25 @@ public class WeekdayDistributionAnalyzer implements
 	public CrosstabResult getResult() {
 		CrosstabDimension columnDimension = new CrosstabDimension("Column");
 		CrosstabDimension weekdayDimension = new CrosstabDimension("Weekday");
-		weekdayDimension.addCategory("Sunday").addCategory("Monday")
-				.addCategory("Tuesday").addCategory("Wednesday")
-				.addCategory("Thursday").addCategory("Friday")
-				.addCategory("Saturday");
-		Crosstab<Integer> crosstab = new Crosstab<Integer>(Integer.class,
-				columnDimension, weekdayDimension);
+		weekdayDimension.addCategory("Sunday").addCategory("Monday").addCategory("Tuesday").addCategory("Wednesday")
+				.addCategory("Thursday").addCategory("Friday").addCategory("Saturday");
+		Crosstab<Integer> crosstab = new Crosstab<Integer>(Integer.class, columnDimension, weekdayDimension);
 		for (InputColumn<Date> col : dateColumns) {
 			columnDimension.addCategory(col.getName());
 			CrosstabNavigator<Integer> nav = crosstab.where(columnDimension, col.getName());
 			Map<Integer, Integer> countMap = distributionMap.get(col);
-			nav.where(weekdayDimension, "Sunday").put(
-					countMap.get(Calendar.SUNDAY));
-			nav.where(weekdayDimension, "Monday").put(
-					countMap.get(Calendar.MONDAY));
-			nav.where(weekdayDimension, "Tuesday").put(
-					countMap.get(Calendar.TUESDAY));
-			nav.where(weekdayDimension, "Wednesday").put(
-					countMap.get(Calendar.WEDNESDAY));
-			nav.where(weekdayDimension, "Thursday").put(
-					countMap.get(Calendar.THURSDAY));
-			nav.where(weekdayDimension, "Friday").put(
-					countMap.get(Calendar.FRIDAY));
-			nav.where(weekdayDimension, "Saturday").put(
-					countMap.get(Calendar.SATURDAY));
+			nav.where(weekdayDimension, "Sunday").put(countMap.get(Calendar.SUNDAY));
+			nav.where(weekdayDimension, "Monday").put(countMap.get(Calendar.MONDAY));
+			nav.where(weekdayDimension, "Tuesday").put(countMap.get(Calendar.TUESDAY));
+			nav.where(weekdayDimension, "Wednesday").put(countMap.get(Calendar.WEDNESDAY));
+			nav.where(weekdayDimension, "Thursday").put(countMap.get(Calendar.THURSDAY));
+			nav.where(weekdayDimension, "Friday").put(countMap.get(Calendar.FRIDAY));
+			nav.where(weekdayDimension, "Saturday").put(countMap.get(Calendar.SATURDAY));
 		}
 
 		return new CrosstabResult(getClass(), crosstab);
 	}
-	
+
 	// used only for unittesting
 	public void setDateColumns(InputColumn<Date>[] dateColumns) {
 		this.dateColumns = dateColumns;
