5a283072732490a7be9d800f27309468417c0ca1
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2010-10-05 06:45:26 +0000

    Removing "user" folder from SCM.

--- user/code/java-classes/foo/MyFunction.java
+++
@@ -1,1 +0,0 @@
-package foo; import org.eobjects.analyzer.reference.Function; public class MyFunction implements Function<String,String> { public String run(String str) { return "" + str.length(); } }
