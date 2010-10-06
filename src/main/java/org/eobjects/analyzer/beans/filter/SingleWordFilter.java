fc8809f812981ef26358c08486f337badcb2f222
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2010-10-06 06:51:28 +0000

    Ticket #415: Added example of filtering in jobs.

---
+++ src/main/java/org/eobjects/analyzer/beans/filter/SingleWordFilter.java
@@ -0,0 +1,32 @@
+package org.eobjects.analyzer.beans.filter;
+
+import org.eobjects.analyzer.beans.api.Configured;
+import org.eobjects.analyzer.beans.api.Filter;
+import org.eobjects.analyzer.beans.api.FilterBean;
+import org.eobjects.analyzer.data.InputColumn;
+import org.eobjects.analyzer.data.InputRow;
+import org.eobjects.analyzer.util.CharIterator;
+
+@FilterBean("Single word")
+public class SingleWordFilter implements Filter<ValidationCategory> {
+
+	@Configured
+	InputColumn<String> input;
+
+	@Override
+	public ValidationCategory categorize(InputRow inputRow) {
+		String value = inputRow.getValue(input);
+		if (value == null || value.length() == 0) {
+			return ValidationCategory.INVALID;
+		}
+		CharIterator it = new CharIterator(value);
+		while (it.hasNext()) {
+			it.next();
+			if (!it.isLetter()) {
+				return ValidationCategory.INVALID;
+			}
+		}
+		return ValidationCategory.VALID;
+	}
+
+}
