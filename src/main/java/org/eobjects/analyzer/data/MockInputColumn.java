69b815834526d1fe7c61813348fa63fb012b1e2f
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2010-10-27 11:32:43 +0000

    javadoc improvement

--- src/main/java/org/eobjects/analyzer/data/MockInputColumn.java
+++ src/main/java/org/eobjects/analyzer/data/MockInputColumn.java
@@ -1,8 +1,15 @@
 package org.eobjects.analyzer.data;
 
-
 import dk.eobjects.metamodel.schema.Column;
 
+/**
+ * A mock-implementation of the input column. Use this only for testing purposes
+ * or in cases where you want to circumvent the actual framework!
+ * 
+ * @author Kasper Sørensen
+ * 
+ * @param <E>
+ */
 public class MockInputColumn<E> extends AbstractInputColumn<E> implements InputColumn<E> {
 
 	private String _name;
--- src/main/java/org/eobjects/analyzer/data/MockInputRow.java
+++ src/main/java/org/eobjects/analyzer/data/MockInputRow.java
@@ -3,14 +3,20 @@ package org.eobjects.analyzer.data;
 import java.util.HashMap;
 import java.util.Map;
 
-
+/**
+ * A mock implementation of the InputRow interface. Allows for adhoc generation
+ * of a row using the put(...) method.
+ * 
+ * @author Kasper Sørensen
+ * 
+ */
 public class MockInputRow implements InputRow {
 
 	Map<InputColumn<?>, Object> map = new HashMap<InputColumn<?>, Object>();
-	
+
 	public MockInputRow() {
 	}
-	
+
 	public MockInputRow(InputColumn<?>[] columns, Object[] values) {
 		for (int i = 0; i < values.length; i++) {
 			put(columns[i], values[i]);
@@ -23,6 +29,13 @@ public class MockInputRow implements InputRow {
 		return (E) map.get(column);
 	}
 
+	/**
+	 * Puts/adds a new value to the row.
+	 * 
+	 * @param column
+	 * @param value
+	 * @return
+	 */
 	public MockInputRow put(InputColumn<?> column, Object value) {
 		map.put(column, value);
 		return this;
