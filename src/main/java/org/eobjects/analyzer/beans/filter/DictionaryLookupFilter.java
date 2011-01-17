232a96c0fdd313c7e158c9127dbee5868bdc1433
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-01-17 10:14:05 +0000

    Added simple filters for lookups in dictionaries and matching in string patterns

---
+++ src/main/java/org/eobjects/analyzer/beans/filter/DictionaryLookupFilter.java
@@ -0,0 +1,51 @@
+/**
+ * eobjects.org AnalyzerBeans
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.analyzer.beans.filter;
+
+import org.eobjects.analyzer.beans.api.Configured;
+import org.eobjects.analyzer.beans.api.Description;
+import org.eobjects.analyzer.beans.api.Filter;
+import org.eobjects.analyzer.beans.api.FilterBean;
+import org.eobjects.analyzer.data.InputColumn;
+import org.eobjects.analyzer.data.InputRow;
+import org.eobjects.analyzer.reference.Dictionary;
+
+@FilterBean("Dictionary lookup")
+@Description("Filters values based on their existence in a dictionary")
+public class DictionaryLookupFilter implements Filter<ValidationCategory> {
+
+	@Configured
+	InputColumn<String> column;
+
+	@Configured
+	Dictionary dictionary;
+
+	@Override
+	public ValidationCategory categorize(InputRow inputRow) {
+		String value = inputRow.getValue(column);
+		if (value != null) {
+			if (dictionary.containsValue(value)) {
+				return ValidationCategory.VALID;
+			}
+		}
+		return ValidationCategory.INVALID;
+	}
+
+}
