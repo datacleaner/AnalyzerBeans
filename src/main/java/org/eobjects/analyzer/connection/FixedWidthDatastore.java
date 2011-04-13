1e625a66d412d34e09e035c01febff2d220f5314
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-04-13 11:04:05 +0000

    Ticket #591: Added datastore class

---
+++ src/main/java/org/eobjects/analyzer/connection/FixedWidthDatastore.java
@@ -0,0 +1,80 @@
+/**
+ * eobjects.org AnalyzerBeans
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.analyzer.connection;
+
+import java.io.File;
+
+import org.eobjects.metamodel.DataContext;
+import org.eobjects.metamodel.DataContextFactory;
+
+/**
+ * Datastore based on fixed width files
+ * 
+ * @author Kasper Sørensen
+ * 
+ */
+public class FixedWidthDatastore extends UsageAwareDatastore implements FileDatastore {
+
+	private static final long serialVersionUID = 1L;
+
+	private final String _name;
+	private final String _filename;
+	private final String _encoding;
+	private final int _fixedValueWidth;
+
+	public FixedWidthDatastore(String name, String filename, String encoding, int fixedValueWidth) {
+		_name = name;
+		_filename = filename;
+		_encoding = encoding;
+		_fixedValueWidth = fixedValueWidth;
+	}
+
+	@Override
+	public String getName() {
+		return _name;
+	}
+
+	@Override
+	public PerformanceCharacteristics getPerformanceCharacteristics() {
+		return new PerformanceCharacteristicsImpl(false);
+	}
+
+	@Override
+	protected UsageAwareDataContextProvider createDataContextProvider() {
+		File file = new File(_filename);
+		assert file.exists();
+
+		DataContext dataContext = DataContextFactory.createFixedWidthDataContext(file, _encoding, _fixedValueWidth);
+		return new SingleDataContextProvider(dataContext, this);
+	}
+
+	public String getEncoding() {
+		return _encoding;
+	}
+
+	public int getFixedValueWidth() {
+		return _fixedValueWidth;
+	}
+
+	@Override
+	public String getFilename() {
+		return _filename;
+	}
+}
---
+++ src/test/java/org/eobjects/analyzer/connection/FixedWidthDatastoreTest.java
@@ -0,0 +1,57 @@
+/**
+ * eobjects.org AnalyzerBeans
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.analyzer.connection;
+
+import java.util.Arrays;
+
+import org.eobjects.metamodel.schema.Schema;
+import org.eobjects.metamodel.schema.Table;
+
+import junit.framework.TestCase;
+
+public class FixedWidthDatastoreTest extends TestCase {
+
+	public void testSimpleGetters() throws Exception {
+		FixedWidthDatastore ds = new FixedWidthDatastore("name", "filename", "encoding", 5);
+		assertEquals("name", ds.getName());
+		assertEquals("filename", ds.getFilename());
+		assertEquals("encoding", ds.getEncoding());
+		assertEquals(5, ds.getFixedValueWidth());
+
+		assertFalse(ds.getPerformanceCharacteristics().isQueryOptimizationPreferred());
+	}
+
+	public void testGetDataContextProvider() throws Exception {
+		FixedWidthDatastore ds = new FixedWidthDatastore("example datastore",
+				"src/test/resources/employees-fixed-width.txt", "UTF-8", 19);
+		
+		DataContextProvider dcp = ds.getDataContextProvider();
+		
+		Schema schema = dcp.getDataContext().getDefaultSchema();
+		assertEquals("employees-fixed-width.txt", schema.getName());
+		
+		Table table = schema.getTables()[0];
+		assertEquals("employees-fixed-width", table.getName());
+		
+		assertEquals("[name, email]", Arrays.toString(table.getColumnNames()));
+		
+		dcp.close();
+	}
+}
