f1d89c83ae08173b2cb52f5be86e5ec84911fb2a
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-05-19 20:23:07 +0000

    Minor improvements to berkeley db map

--- src/main/java/org/eobjects/analyzer/storage/BerkeleyDbMap.java
+++ src/main/java/org/eobjects/analyzer/storage/BerkeleyDbMap.java
@@ -23,11 +23,16 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sleepycat.collections.StoredMap;
 import com.sleepycat.je.Database;
 import com.sleepycat.je.Environment;
 
 final class BerkeleyDbMap<K, V> implements Map<K, V> {
+
+	private static final Logger logger = LoggerFactory.getLogger(BerkeleyDbMap.class);
 
 	private final Map<K, V> _wrappedMap;
 	private final Database _database;
@@ -48,64 +53,83 @@ final class BerkeleyDbMap<K, V> implements Map<K, V> {
 		_environment.removeDatabase(null, name);
 	}
 
+	@Override
 	public int size() {
 		return _wrappedMap.size();
 	}
 
+	@Override
 	public boolean isEmpty() {
 		return _wrappedMap.isEmpty();
 	}
 
+	@Override
 	public boolean containsKey(Object key) {
 		return _wrappedMap.containsKey(key);
 	}
 
+	@Override
 	public boolean containsValue(Object value) {
 		return _wrappedMap.containsValue(value);
 	}
 
+	@Override
 	public V get(Object key) {
 		try {
 			return _wrappedMap.get(key);
 		} catch (ArrayIndexOutOfBoundsException e) {
+			logger.warn("get('{}') threw exception: {}", key, e.getMessage());
+			logger.warn("Swallowing exception, returning null", e);
 			// there's a bug in berkeley that sometime causes this exception
 			// when the value is null!
 			return null;
 		}
 	}
 
+	@Override
 	public V put(K key, V value) {
 		return _wrappedMap.put(key, value);
 	}
 
+	@Override
 	public V remove(Object key) {
 		return _wrappedMap.remove(key);
 	}
 
+	@Override
 	public void putAll(Map<? extends K, ? extends V> m) {
 		_wrappedMap.putAll(m);
 	}
 
+	@Override
 	public void clear() {
 		_wrappedMap.clear();
 	}
 
+	@Override
 	public Set<K> keySet() {
 		return _wrappedMap.keySet();
 	}
 
+	@Override
 	public Collection<V> values() {
 		return _wrappedMap.values();
 	}
 
+	@Override
 	public Set<java.util.Map.Entry<K, V>> entrySet() {
 		return _wrappedMap.entrySet();
 	}
 
+	@Override
 	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
 		return _wrappedMap.equals(o);
 	}
 
+	@Override
 	public int hashCode() {
 		return _wrappedMap.hashCode();
 	}
