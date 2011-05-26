a8d2871f9bc640e6107e587ce240043e41675df6
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-05-26 07:12:17 +0000

    Added one more unittest for predefined tokens

--- src/test/java/org/eobjects/analyzer/beans/stringpattern/PredefinedTokenTokenizerTest.java
+++ src/test/java/org/eobjects/analyzer/beans/stringpattern/PredefinedTokenTokenizerTest.java
@@ -28,8 +28,7 @@ import junit.framework.TestCase;
 public class PredefinedTokenTokenizerTest extends TestCase {
 
 	public void testOverlappingPatterns() throws Exception {
-		PredefinedTokenDefinition pt = new PredefinedTokenDefinition(
-				"greeting", "hello .*", "hi .*");
+		PredefinedTokenDefinition pt = new PredefinedTokenDefinition("greeting", "hello .*", "hi .*");
 
 		Set<Pattern> patterns = pt.getTokenRegexPatterns();
 		assertEquals(2, patterns.size());
@@ -38,40 +37,55 @@ public class PredefinedTokenTokenizerTest extends TestCase {
 			assertTrue(pattern.matcher("hello hi there").find());
 		}
 
-		List<Token> tokens = new PredefinedTokenTokenizer(pt)
-				.tokenize("hello hi there");
+		List<Token> tokens = new PredefinedTokenTokenizer(pt).tokenize("hello hi there");
 		assertEquals(2, tokens.size());
 		assertEquals("UndefinedToken['hello ']", tokens.get(0).toString());
-		assertEquals("Token['hi there' (PREDEFINED greeting)]", tokens.get(1)
-				.toString());
+		assertEquals("Token['hi there' (PREDEFINED greeting)]", tokens.get(1).toString());
+	}
+
+	public void testTitulation() throws Exception {
+		PredefinedTokenDefinition pt = new PredefinedTokenDefinition("titulation", "(Mr|Ms)\\.");
+		List<Token> tokens;
+
+		tokens = PredefinedTokenTokenizer.tokenizeInternal("Mr. Kasper", pt, pt.getTokenRegexPatterns().iterator().next());
+		assertEquals(2, tokens.size());
+		assertEquals("Token['Mr.' (PREDEFINED titulation)]", tokens.get(0).toString());
+		assertEquals("UndefinedToken[' Kasper']", tokens.get(1).toString());
+
+		tokens = PredefinedTokenTokenizer.tokenizeInternal("Dear Mr. Winfried", pt, pt.getTokenRegexPatterns().iterator()
+				.next());
+		assertEquals(3, tokens.size());
+		assertEquals("UndefinedToken['Dear ']", tokens.get(0).toString());
+		assertEquals("Token['Mr.' (PREDEFINED titulation)]", tokens.get(1).toString());
+		assertEquals("UndefinedToken[' Winfried']", tokens.get(2).toString());
+
+		tokens = PredefinedTokenTokenizer.tokenizeInternal("Dear Ms. Barbara", pt, pt.getTokenRegexPatterns().iterator()
+				.next());
+		assertEquals(3, tokens.size());
+		assertEquals("UndefinedToken['Dear ']", tokens.get(0).toString());
+		assertEquals("Token['Ms.' (PREDEFINED titulation)]", tokens.get(1).toString());
+		assertEquals("UndefinedToken[' Barbara']", tokens.get(2).toString());
 	}
 
 	public void testTokenizeInternal() throws Exception {
-		PredefinedTokenDefinition pt = new PredefinedTokenDefinition(
-				"greeting", "hello");
-		List<Token> tokens = PredefinedTokenTokenizer.tokenizeInternal(
-				"hello there hello world", pt, pt.getTokenRegexPatterns()
-						.iterator().next());
+		PredefinedTokenDefinition pt = new PredefinedTokenDefinition("greeting", "hello");
+		List<Token> tokens = PredefinedTokenTokenizer.tokenizeInternal("hello there hello world", pt, pt
+				.getTokenRegexPatterns().iterator().next());
 		assertEquals(4, tokens.size());
 
-		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(0)
-				.toString());
+		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(0).toString());
 		assertEquals("UndefinedToken[' there ']", tokens.get(1).toString());
-		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(2)
-				.toString());
+		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(2).toString());
 		assertEquals("UndefinedToken[' world']", tokens.get(3).toString());
 
-		tokens = PredefinedTokenTokenizer.tokenizeInternal("world hello", pt,
-				pt.getTokenRegexPatterns().iterator().next());
+		tokens = PredefinedTokenTokenizer.tokenizeInternal("world hello", pt, pt.getTokenRegexPatterns().iterator().next());
 		assertEquals(2, tokens.size());
 		assertEquals("UndefinedToken['world ']", tokens.get(0).toString());
-		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(1)
-				.toString());
+		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(1).toString());
 	}
 
 	public void testSimpleTokenSeparation() throws Exception {
-		PredefinedTokenDefinition pt = new PredefinedTokenDefinition(
-				"greeting", "hi", "hello", "howdy");
+		PredefinedTokenDefinition pt = new PredefinedTokenDefinition("greeting", "hi", "hello", "howdy");
 
 		PredefinedTokenTokenizer tokenizer = new PredefinedTokenTokenizer(pt);
 
@@ -79,20 +93,15 @@ public class PredefinedTokenTokenizerTest extends TestCase {
 		assertEquals(3, tokens.size());
 
 		assertEquals("UndefinedToken['Well ']", tokens.get(0).toString());
-		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(1)
-				.toString());
+		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(1).toString());
 		assertEquals("UndefinedToken[' there world']", tokens.get(2).toString());
 
 		tokens = tokenizer.tokenize("howdy Well hello there hi world hi");
 		assertEquals(7, tokens.size());
-		assertEquals("Token['howdy' (PREDEFINED greeting)]", tokens.get(0)
-				.toString());
-		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(2)
-				.toString());
-		assertEquals("Token['hi' (PREDEFINED greeting)]", tokens.get(4)
-				.toString());
+		assertEquals("Token['howdy' (PREDEFINED greeting)]", tokens.get(0).toString());
+		assertEquals("Token['hello' (PREDEFINED greeting)]", tokens.get(2).toString());
+		assertEquals("Token['hi' (PREDEFINED greeting)]", tokens.get(4).toString());
 		assertEquals("UndefinedToken[' world ']", tokens.get(5).toString());
-		assertEquals("Token['hi' (PREDEFINED greeting)]", tokens.get(6)
-				.toString());
+		assertEquals("Token['hi' (PREDEFINED greeting)]", tokens.get(6).toString());
 	}
 }
