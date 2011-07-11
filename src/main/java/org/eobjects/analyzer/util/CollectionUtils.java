8339af544466c25e5736c8e18c98b0b9d6623a45
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2011-07-11 08:25:38 +0000

    Moved Function class to util

--- src/main/java/org/eobjects/analyzer/util/CollectionUtils.java
+++ src/main/java/org/eobjects/analyzer/util/CollectionUtils.java
@@ -30,7 +30,6 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.collections15.map.ReferenceMap;
-import org.eobjects.analyzer.reference.Function;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- src/main/java/org/eobjects/analyzer/util/Function.java
+++ src/main/java/org/eobjects/analyzer/util/Function.java
@@ -17,7 +17,7 @@
  * 51 Franklin Street, Fifth Floor
  * Boston, MA  02110-1301  USA
  */
-package org.eobjects.analyzer.reference;
+package org.eobjects.analyzer.util;
 
 import java.io.Serializable;
 
