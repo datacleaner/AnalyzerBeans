2aef1f9eda60a283c1f17ab93c7a767b0338d028
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-01-19 19:52:31 +0000

    Fixed unittests

--- core/src/test/java/org/eobjects/analyzer/result/renderer/CrosstabRendererTest.java
+++ core/src/test/java/org/eobjects/analyzer/result/renderer/CrosstabRendererTest.java
@@ -35,12 +35,12 @@ public class CrosstabRendererTest extends TestCase {
 
 		CrosstabRenderer crosstabRenderer = new CrosstabRenderer(c);
 		String result = crosstabRenderer.render(new HtmlCrosstabRendererCallback());
-		assertEquals("<table><tr><td>EU</td><td>USA</td><td>Asia</td></tr>"
+		assertEquals("<table class='crosstabTable'><tr><td>EU</td><td>USA</td><td>Asia</td></tr>"
 				+ "<tr><td>1</td><td>2</td><td>3</td></tr></table>", result.replaceAll("\"", "'"));
 
 		crosstabRenderer.makeVertical(c.getDimension(0));
 		result = crosstabRenderer.render(new HtmlCrosstabRendererCallback());
-		assertEquals("<table><tr><td>EU</td><td>1</td></tr>" + "<tr><td>USA</td><td>2</td></tr>"
+		assertEquals("<table class='crosstabTable'><tr><td>EU</td><td>1</td></tr>" + "<tr><td>USA</td><td>2</td></tr>"
 				+ "<tr><td>Asia</td><td>3</td></tr></table>", result.replaceAll("\"", "'"));
 	}
 
@@ -76,12 +76,12 @@ public class CrosstabRendererTest extends TestCase {
 
 		// auto-assigned axises
 		assertEquals(
-				"<table><tr><td></td><td></td><td colspan='3'>EU</td><td colspan='3'>USA</td></tr>"
+				"<table class='crosstabTable'><tr><td></td><td></td><td class='crosstabHorizontalHeader' colspan='3'>EU</td><td class='crosstabHorizontalHeader' colspan='3'>USA</td></tr>"
 						+ "<tr><td></td><td></td><td>Child</td><td>Teenager</td><td>Adult</td><td>Child</td><td>Teenager</td><td>Adult</td></tr>"
-						+ "<tr><td rowspan='3'>Male</td><td>Yes</td><td>0</td><td>3</td><td>6</td><td>9</td><td>12</td><td>15</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='3'>Male</td><td>Yes</td><td>0</td><td>3</td><td>6</td><td>9</td><td>12</td><td>15</td></tr>"
 						+ "<tr><td>No, immigrant</td><td>1</td><td>4</td><td>7</td><td>10</td><td>13</td><td>16</td></tr>"
 						+ "<tr><td>No, second-generation</td><td>2</td><td>5</td><td>8</td><td>11</td><td>14</td><td>17</td></tr>"
-						+ "<tr><td rowspan='3'>Female</td><td>Yes</td><td>18</td><td>21</td><td>24</td><td>27</td><td>30</td><td>33</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='3'>Female</td><td>Yes</td><td>18</td><td>21</td><td>24</td><td>27</td><td>30</td><td>33</td></tr>"
 						+ "<tr><td>No, immigrant</td><td>19</td><td>22</td><td>25</td><td>28</td><td>31</td><td>34</td></tr>"
 						+ "<tr><td>No, second-generation</td><td>20</td><td>23</td><td>26</td><td>29</td><td>32</td><td>35</td></tr></table>",
 				crosstabRenderer.render(new HtmlCrosstabRendererCallback()).replaceAll("\"", "'"));
@@ -92,39 +92,40 @@ public class CrosstabRendererTest extends TestCase {
 		crosstabRenderer.makeVertical(c.getDimension(2));
 		crosstabRenderer.makeVertical(c.getDimension(3));
 		assertEquals(
-				"<table><tr><td rowspan='18'>Male</td><td rowspan='6'>Yes</td><td rowspan='3'>EU</td><td>Child</td><td>0</td></tr>"
+				"<table class='crosstabTable'><tr><td class='crosstabVerticalHeader' rowspan='18'>Male</td><td class='crosstabVerticalHeader' rowspan='6'>Yes</td><td class='crosstabVerticalHeader' rowspan='3'>EU</td><td>Child</td><td>0</td></tr>"
 						+ "<tr><td>Teenager</td><td>3</td></tr>"
 						+ "<tr><td>Adult</td><td>6</td></tr>"
-						+ "<tr><td rowspan='3'>USA</td><td>Child</td><td>9</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='3'>USA</td><td>Child</td><td>9</td></tr>"
 						+ "<tr><td>Teenager</td><td>12</td></tr>"
 						+ "<tr><td>Adult</td><td>15</td></tr>"
-						+ "<tr><td rowspan='6'>No, immigrant</td><td rowspan='3'>EU</td><td>Child</td><td>1</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='6'>No, immigrant</td><td class='crosstabVerticalHeader' rowspan='3'>EU</td><td>Child</td><td>1</td></tr>"
 						+ "<tr><td>Teenager</td><td>4</td></tr>"
 						+ "<tr><td>Adult</td><td>7</td></tr>"
-						+ "<tr><td rowspan='3'>USA</td><td>Child</td><td>10</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='3'>USA</td><td>Child</td><td>10</td></tr>"
 						+ "<tr><td>Teenager</td><td>13</td></tr>"
 						+ "<tr><td>Adult</td><td>16</td></tr>"
-						+ "<tr><td rowspan='6'>No, second-generation</td><td rowspan='3'>EU</td><td>Child</td><td>2</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='6'>No, second-generation</td><td class='crosstabVerticalHeader' rowspan='3'>EU</td><td>Child</td><td>2</td></tr>"
 						+ "<tr><td>Teenager</td><td>5</td></tr>"
 						+ "<tr><td>Adult</td><td>8</td></tr>"
-						+ "<tr><td rowspan='3'>USA</td><td>Child</td><td>11</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='3'>USA</td><td>Child</td><td>11</td></tr>"
 						+ "<tr><td>Teenager</td><td>14</td></tr>"
 						+ "<tr><td>Adult</td><td>17</td></tr>"
-						+ "<tr><td rowspan='18'>Female</td><td rowspan='6'>Yes</td><td rowspan='3'>EU</td><td>Child</td><td>18</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='18'>Female</td><td class='crosstabVerticalHeader' rowspan='6'>Yes</td><td class='crosstabVerticalHeader' rowspan='3'>EU</td><td>Child</td><td>18</td></tr>"
 						+ "<tr><td>Teenager</td><td>21</td></tr>"
 						+ "<tr><td>Adult</td><td>24</td></tr>"
-						+ "<tr><td rowspan='3'>USA</td><td>Child</td><td>27</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='3'>USA</td><td>Child</td><td>27</td></tr>"
 						+ "<tr><td>Teenager</td><td>30</td></tr>"
 						+ "<tr><td>Adult</td><td>33</td></tr>"
-						+ "<tr><td rowspan='6'>No, immigrant</td><td rowspan='3'>EU</td><td>Child</td><td>19</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='6'>No, immigrant</td><td class='crosstabVerticalHeader' rowspan='3'>EU</td><td>Child</td><td>19</td></tr>"
 						+ "<tr><td>Teenager</td><td>22</td></tr>"
 						+ "<tr><td>Adult</td><td>25</td></tr>"
-						+ "<tr><td rowspan='3'>USA</td><td>Child</td><td>28</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='3'>USA</td><td>Child</td><td>28</td></tr>"
 						+ "<tr><td>Teenager</td><td>31</td></tr>"
 						+ "<tr><td>Adult</td><td>34</td></tr>"
-						+ "<tr><td rowspan='6'>No, second-generation</td><td rowspan='3'>EU</td><td>Child</td><td>20</td></tr>"
-						+ "<tr><td>Teenager</td><td>23</td></tr>" + "<tr><td>Adult</td><td>26</td></tr>"
-						+ "<tr><td rowspan='3'>USA</td><td>Child</td><td>29</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='6'>No, second-generation</td><td class='crosstabVerticalHeader' rowspan='3'>EU</td><td>Child</td><td>20</td></tr>"
+						+ "<tr><td>Teenager</td><td>23</td></tr>"
+						+ "<tr><td>Adult</td><td>26</td></tr>"
+						+ "<tr><td class='crosstabVerticalHeader' rowspan='3'>USA</td><td>Child</td><td>29</td></tr>"
 						+ "<tr><td>Teenager</td><td>32</td></tr>" + "<tr><td>Adult</td><td>35</td></tr></table>",
 				crosstabRenderer.render(new HtmlCrosstabRendererCallback()).replaceAll("\"", "'"));
 	}
@@ -134,7 +135,7 @@ public class CrosstabRendererTest extends TestCase {
 
 		CrosstabTextRenderer renderer = new CrosstabTextRenderer();
 		String result = renderer.render(crosstab);
-		
+
 		assertEquals("", result);
 	}
 }
