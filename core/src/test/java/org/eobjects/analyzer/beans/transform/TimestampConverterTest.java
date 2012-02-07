0ee1a949bb445d8ded6ab2301ea4010449f328bf
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-02-07 09:30:06 +0000

    Made unittest locale-independent

--- core/src/test/java/org/eobjects/analyzer/beans/transform/TimestampConverterTest.java
+++ core/src/test/java/org/eobjects/analyzer/beans/transform/TimestampConverterTest.java
@@ -21,6 +21,7 @@ package org.eobjects.analyzer.beans.transform;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.eobjects.analyzer.beans.transform.TimestampConverter.Unit;
 import org.eobjects.analyzer.data.MockInputColumn;
@@ -30,33 +31,46 @@ import junit.framework.TestCase;
 
 public class TimestampConverterTest extends TestCase {
 
+	private TimeZone _defaultTimeZone;
+
+	protected void setUp() throws Exception {
+		_defaultTimeZone = TimeZone.getDefault();
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	};
+
+	@Override
+	protected void tearDown() throws Exception {
+		TimeZone.setDefault(_defaultTimeZone);
+	}
+
 	public void testTransform() throws Exception {
 		TimestampConverter trans = new TimestampConverter();
-		MockInputColumn<Object> col = new MockInputColumn<Object>("my timestamps", Object.class);
+		MockInputColumn<Object> col = new MockInputColumn<Object>(
+				"my timestamps", Object.class);
 		trans.timestampColumn = col;
-		
+
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-		
+
 		Date[] result;
 		result = trans.transform(new MockInputRow().put(col, "1320244696"));
 		assertEquals(1, result.length);
-		assertEquals("2011-11-02 15:38", dateFormat.format(result[0]));
-		
+		assertEquals("2011-11-02 14:38", dateFormat.format(result[0]));
+
 		result = trans.transform(new MockInputRow().put(col, 1234));
 		assertEquals(1, result.length);
-		assertEquals("1970-01-01 01:20", dateFormat.format(result[0]));
-		
+		assertEquals("1970-01-01 00:20", dateFormat.format(result[0]));
+
 		result = trans.transform(new MockInputRow().put(col, null));
 		assertEquals(1, result.length);
 		assertNull(result[0]);
-		
+
 		result = trans.transform(new MockInputRow().put(col, "foobar"));
 		assertEquals(1, result.length);
 		assertNull(result[0]);
-		
+
 		trans.unit = Unit.DAYS;
 		result = trans.transform(new MockInputRow().put(col, 20));
 		assertEquals(1, result.length);
-		assertEquals("1970-01-21 01:00", dateFormat.format(result[0]));
+		assertEquals("1970-01-21 00:00", dateFormat.format(result[0]));
 	}
 }
