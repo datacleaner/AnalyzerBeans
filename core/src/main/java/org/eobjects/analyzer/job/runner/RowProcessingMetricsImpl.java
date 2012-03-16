788a13c37e9b3c6af49fcdbd42ed1f82cef86402
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-03-16 07:33:38 +0000

    fixed closing of dataset when counting row metrics

--- core/src/main/java/org/eobjects/analyzer/job/runner/RowProcessingMetricsImpl.java
+++ core/src/main/java/org/eobjects/analyzer/job/runner/RowProcessingMetricsImpl.java
@@ -31,83 +31,88 @@ import org.eobjects.metamodel.util.Ref;
 
 final class RowProcessingMetricsImpl implements RowProcessingMetrics {
 
-	private final AnalysisJobMetrics _analysisJobMetrics;
-	private final Ref<Query> _queryRef;
-	private final Ref<Integer> _expectedRows;
-	private final Table _table;
-	private final AnalyzerJob[] _analyzerJobs;
+    private final AnalysisJobMetrics _analysisJobMetrics;
+    private final Ref<Query> _queryRef;
+    private final Ref<Integer> _expectedRows;
+    private final Table _table;
+    private final AnalyzerJob[] _analyzerJobs;
 
-	public RowProcessingMetricsImpl(AnalysisJobMetrics analysisJobMetrics, Table table, AnalyzerJob[] analyzerJobs,
-			Ref<Query> queryRef) {
-		_analysisJobMetrics = analysisJobMetrics;
-		_table = table;
-		_analyzerJobs = analyzerJobs;
-		_queryRef = queryRef;
-		_expectedRows = createExpectedRowsRef();
-	}
+    public RowProcessingMetricsImpl(AnalysisJobMetrics analysisJobMetrics, Table table, AnalyzerJob[] analyzerJobs,
+            Ref<Query> queryRef) {
+        _analysisJobMetrics = analysisJobMetrics;
+        _table = table;
+        _analyzerJobs = analyzerJobs;
+        _queryRef = queryRef;
+        _expectedRows = createExpectedRowsRef();
+    }
 
-	@Override
-	public AnalysisJobMetrics getAnalysisJobMetrics() {
-		return _analysisJobMetrics;
-	}
+    @Override
+    public AnalysisJobMetrics getAnalysisJobMetrics() {
+        return _analysisJobMetrics;
+    }
 
-	@Override
-	public Query getQuery() {
-		return _queryRef.get().clone();
-	}
+    @Override
+    public Query getQuery() {
+        return _queryRef.get().clone();
+    }
 
-	@Override
-	public Table getTable() {
-		return _table;
-	}
+    @Override
+    public Table getTable() {
+        return _table;
+    }
 
-	@Override
-	public int getExpectedRows() {
-		return _expectedRows.get().intValue();
-	}
+    @Override
+    public int getExpectedRows() {
+        return _expectedRows.get().intValue();
+    }
 
-	@Override
-	public AnalyzerJob[] getAnalyzerJobs() {
-		return _analyzerJobs;
-	}
+    @Override
+    public AnalyzerJob[] getAnalyzerJobs() {
+        return _analyzerJobs;
+    }
 
-	private Ref<Integer> createExpectedRowsRef() {
-		return new LazyRef<Integer>() {
+    private Ref<Integer> createExpectedRowsRef() {
+        return new LazyRef<Integer>() {
 
-			@Override
-			protected Integer fetch() {
-				int expectedRows = -1;
-				{
-					Query query = getQuery();
-					final Query countQuery = query.clone();
-					countQuery.setMaxRows(null);
-					countQuery.getSelectClause().removeItems();
-					countQuery.selectCount();
-					countQuery.getSelectClause().getItem(0).setFunctionApproximationAllowed(true);
+            @Override
+            protected Integer fetch() {
+                int expectedRows = -1;
+                {
+                    Query query = getQuery();
+                    final Query countQuery = query.clone();
+                    countQuery.setMaxRows(null);
+                    countQuery.getSelectClause().removeItems();
+                    countQuery.selectCount();
+                    countQuery.getSelectClause().getItem(0).setFunctionApproximationAllowed(true);
 
-					Datastore datastore = _analysisJobMetrics.getAnalysisJob().getDatastore();
-					DatastoreConnection connection = datastore.openConnection();
-					try {
-						final DataSet countDataSet = connection.getDataContext().executeQuery(countQuery);
-						if (countDataSet.next()) {
-							Number count = ConvertToNumberTransformer.transformValue(countDataSet.getRow().getValue(0));
-							if (count != null) {
-								expectedRows = count.intValue();
-							}
-						}
-					} finally {
-						connection.close();
-					}
+                    Datastore datastore = _analysisJobMetrics.getAnalysisJob().getDatastore();
+                    DatastoreConnection connection = datastore.openConnection();
+                    try {
+                        final DataSet countDataSet = connection.getDataContext().executeQuery(countQuery);
+                        try {
+                            if (countDataSet.next()) {
+                                Number count = ConvertToNumberTransformer.transformValue(countDataSet.getRow()
+                                        .getValue(0));
+                                if (count != null) {
+                                    expectedRows = count.intValue();
+                                }
+                            }
+                        } finally {
+                            countDataSet.close();
+                        }
+                    } finally {
+                        connection.close();
+                    }
 
-					Integer maxRows = query.getMaxRows();
-					if (maxRows != null) {
-						expectedRows = Math.min(expectedRows, maxRows.intValue());
-					}
-				}
+                    Integer maxRows = query.getMaxRows();
+                    if (maxRows != null) {
+                        expectedRows = Math.min(expectedRows, maxRows.intValue());
+                    }
+                }
 
-				return expectedRows;
-			}
-		};
-	}
+                return expectedRows;
+            }
+        };
+    }
 
 }
