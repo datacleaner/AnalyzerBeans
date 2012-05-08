04b44c41e4d82e63dfa5d908a0cc3f6f52ea6ccd
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-05-08 19:25:17 +0000

    added unittest

--- core/src/test/java/org/eobjects/analyzer/util/CollectionUtilsTest.java
+++ core/src/test/java/org/eobjects/analyzer/util/CollectionUtilsTest.java
@@ -21,6 +21,7 @@ package org.eobjects.analyzer.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -30,42 +31,52 @@ import org.eobjects.metamodel.util.ToStringComparator;
 
 public class CollectionUtilsTest extends TestCase {
 
-	public void testArray1() throws Exception {
-		String[] result = CollectionUtils.array(new String[] { "foo", "bar" }, "hello", "world");
-		assertEquals("[foo, bar, hello, world]", Arrays.toString(result));
-	}
+    public void testArray1() throws Exception {
+        String[] result = CollectionUtils.array(new String[] { "foo", "bar" }, "hello", "world");
+        assertEquals("[foo, bar, hello, world]", Arrays.toString(result));
+    }
 
-	public void testArray2() throws Exception {
-		Object existingArray = new Object[] { 'c' };
-		Object[] result = CollectionUtils2.array(Object.class, existingArray, "foo", 1, "bar");
+    public void testArray2() throws Exception {
+        Object existingArray = new Object[] { 'c' };
+        Object[] result = CollectionUtils2.array(Object.class, existingArray, "foo", 1, "bar");
 
-		assertEquals("[c, foo, 1, bar]", Arrays.toString(result));
-	}
+        assertEquals("[c, foo, 1, bar]", Arrays.toString(result));
+    }
 
-	public void testSorted() throws Exception {
-		ArrayList<String> list1 = new ArrayList<String>();
-		list1.add("4");
-		list1.add("1");
-		list1.add("3");
-		list1.add("2");
+    public void testFilterOnClass() throws Exception {
+        Collection<Object> superTypeList = new ArrayList<Object>();
+        superTypeList.add(123);
+        superTypeList.add("foo");
+        superTypeList.add(123);
+        superTypeList.add("bar");
+        List<String> result = CollectionUtils2.filterOnClass(superTypeList, String.class);
+        assertEquals("[foo, bar]", result.toString());
+    }
 
-		List<String> list2 = CollectionUtils2.sorted(list1, ToStringComparator.getComparator());
-		assertEquals("[4, 1, 3, 2]", list1.toString());
-		assertEquals("[1, 2, 3, 4]", list2.toString());
-	}
+    public void testSorted() throws Exception {
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("4");
+        list1.add("1");
+        list1.add("3");
+        list1.add("2");
 
-	public void testArrayRemove() throws Exception {
-		String[] arr = new String[] { "a", "b", "c", "d", "e" };
-		arr = CollectionUtils.arrayRemove(arr, "c");
-		assertEquals("[a, b, d, e]", Arrays.toString(arr));
+        List<String> list2 = CollectionUtils2.sorted(list1, ToStringComparator.getComparator());
+        assertEquals("[4, 1, 3, 2]", list1.toString());
+        assertEquals("[1, 2, 3, 4]", list2.toString());
+    }
 
-		arr = CollectionUtils.arrayRemove(arr, "e");
-		assertEquals("[a, b, d]", Arrays.toString(arr));
+    public void testArrayRemove() throws Exception {
+        String[] arr = new String[] { "a", "b", "c", "d", "e" };
+        arr = CollectionUtils.arrayRemove(arr, "c");
+        assertEquals("[a, b, d, e]", Arrays.toString(arr));
 
-		arr = CollectionUtils.arrayRemove(arr, "e");
-		assertEquals("[a, b, d]", Arrays.toString(arr));
+        arr = CollectionUtils.arrayRemove(arr, "e");
+        assertEquals("[a, b, d]", Arrays.toString(arr));
 
-		arr = CollectionUtils.arrayRemove(arr, "a");
-		assertEquals("[b, d]", Arrays.toString(arr));
-	}
+        arr = CollectionUtils.arrayRemove(arr, "e");
+        assertEquals("[a, b, d]", Arrays.toString(arr));
+
+        arr = CollectionUtils.arrayRemove(arr, "a");
+        assertEquals("[b, d]", Arrays.toString(arr));
+    }
 }
