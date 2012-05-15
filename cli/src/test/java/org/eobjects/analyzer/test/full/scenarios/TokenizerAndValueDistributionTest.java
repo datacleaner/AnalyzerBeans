3b42701b73f9735686d2cb874bcc3175964f89df
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-05-15 10:16:56 +0000

    fixed import

--- cli/src/test/java/org/eobjects/analyzer/test/full/scenarios/TokenizerAndValueDistributionTest.java
+++ cli/src/test/java/org/eobjects/analyzer/test/full/scenarios/TokenizerAndValueDistributionTest.java
@@ -25,7 +25,7 @@ import java.util.TreeSet;
 
 import junit.framework.TestCase;
 
-import org.eobjects.analyzer.beans.standardize.TokenizerTransformer;
+import org.eobjects.analyzer.beans.transform.TokenizerTransformer;
 import org.eobjects.analyzer.beans.valuedist.ValueDistributionAnalyzer;
 import org.eobjects.analyzer.configuration.AnalyzerBeansConfiguration;
 import org.eobjects.analyzer.configuration.AnalyzerBeansConfigurationImpl;
@@ -52,101 +52,101 @@ import org.eobjects.metamodel.schema.Table;
 
 public class TokenizerAndValueDistributionTest extends TestCase {
 
-	public void testScenario() throws Throwable {
-		TaskRunner taskRunner = new MultiThreadedTaskRunner(5);
+    public void testScenario() throws Throwable {
+        TaskRunner taskRunner = new MultiThreadedTaskRunner(5);
 
-		AnalyzerBeansConfiguration configuration = new AnalyzerBeansConfigurationImpl().replace(taskRunner);
+        AnalyzerBeansConfiguration configuration = new AnalyzerBeansConfigurationImpl().replace(taskRunner);
 
-		AnalysisRunner runner = new AnalysisRunnerImpl(configuration);
+        AnalysisRunner runner = new AnalysisRunnerImpl(configuration);
 
-		Datastore datastore = TestHelper.createSampleDatabaseDatastore("ds");
-		DatastoreConnection con = datastore.openConnection();
-		DataContext dc = con.getDataContext();
+        Datastore datastore = TestHelper.createSampleDatabaseDatastore("ds");
+        DatastoreConnection con = datastore.openConnection();
+        DataContext dc = con.getDataContext();
 
-		AnalysisJobBuilder analysisJobBuilder = new AnalysisJobBuilder(configuration);
-		analysisJobBuilder.setDatastoreConnection(con);
+        AnalysisJobBuilder analysisJobBuilder = new AnalysisJobBuilder(configuration);
+        analysisJobBuilder.setDatastoreConnection(con);
 
-		Table table = dc.getDefaultSchema().getTableByName("EMPLOYEES");
-		assertNotNull(table);
+        Table table = dc.getDefaultSchema().getTableByName("EMPLOYEES");
+        assertNotNull(table);
 
-		Column jobTitleColumn = table.getColumnByName("JOBTITLE");
-		assertNotNull(jobTitleColumn);
+        Column jobTitleColumn = table.getColumnByName("JOBTITLE");
+        assertNotNull(jobTitleColumn);
 
-		analysisJobBuilder.addSourceColumns(jobTitleColumn);
+        analysisJobBuilder.addSourceColumns(jobTitleColumn);
 
-		TransformerJobBuilder<TokenizerTransformer> transformerJobBuilder = analysisJobBuilder
-				.addTransformer(TokenizerTransformer.class);
-		transformerJobBuilder.addInputColumn(analysisJobBuilder.getSourceColumns().get(0));
-		transformerJobBuilder.setConfiguredProperty("Number of tokens", 4);
-		List<MutableInputColumn<?>> transformerOutput = transformerJobBuilder.getOutputColumns();
-		assertEquals(4, transformerOutput.size());
+        TransformerJobBuilder<TokenizerTransformer> transformerJobBuilder = analysisJobBuilder
+                .addTransformer(TokenizerTransformer.class);
+        transformerJobBuilder.addInputColumn(analysisJobBuilder.getSourceColumns().get(0));
+        transformerJobBuilder.setConfiguredProperty("Number of tokens", 4);
+        List<MutableInputColumn<?>> transformerOutput = transformerJobBuilder.getOutputColumns();
+        assertEquals(4, transformerOutput.size());
 
-		transformerOutput.get(0).setName("first word");
-		transformerOutput.get(1).setName("second word");
-		transformerOutput.get(2).setName("third words");
-		transformerOutput.get(3).setName("fourth words");
+        transformerOutput.get(0).setName("first word");
+        transformerOutput.get(1).setName("second word");
+        transformerOutput.get(2).setName("third words");
+        transformerOutput.get(3).setName("fourth words");
 
-		for (InputColumn<?> inputColumn : transformerOutput) {
-			AnalyzerJobBuilder<ValueDistributionAnalyzer> valueDistribuitionJobBuilder = analysisJobBuilder
-					.addAnalyzer(ValueDistributionAnalyzer.class);
-			valueDistribuitionJobBuilder.addInputColumn(inputColumn);
-			valueDistribuitionJobBuilder.setConfiguredProperty("Record unique values", true);
-			valueDistribuitionJobBuilder.setConfiguredProperty("Top n most frequent values", null);
-			valueDistribuitionJobBuilder.setConfiguredProperty("Bottom n most frequent values", null);
-		}
+        for (InputColumn<?> inputColumn : transformerOutput) {
+            AnalyzerJobBuilder<ValueDistributionAnalyzer> valueDistribuitionJobBuilder = analysisJobBuilder
+                    .addAnalyzer(ValueDistributionAnalyzer.class);
+            valueDistribuitionJobBuilder.addInputColumn(inputColumn);
+            valueDistribuitionJobBuilder.setConfiguredProperty("Record unique values", true);
+            valueDistribuitionJobBuilder.setConfiguredProperty("Top n most frequent values", null);
+            valueDistribuitionJobBuilder.setConfiguredProperty("Bottom n most frequent values", null);
+        }
 
-		AnalysisJob analysisJob = analysisJobBuilder.toAnalysisJob();
+        AnalysisJob analysisJob = analysisJobBuilder.toAnalysisJob();
 
-		AnalysisResultFuture resultFuture = runner.run(analysisJob);
+        AnalysisResultFuture resultFuture = runner.run(analysisJob);
 
-		assertFalse(resultFuture.isDone());
+        assertFalse(resultFuture.isDone());
 
-		List<AnalyzerResult> results = resultFuture.getResults();
+        List<AnalyzerResult> results = resultFuture.getResults();
 
-		assertTrue(resultFuture.isDone());
+        assertTrue(resultFuture.isDone());
 
-		if (!resultFuture.isSuccessful()) {
-			List<Throwable> errors = resultFuture.getErrors();
-			throw errors.get(0);
-		}
+        if (!resultFuture.isSuccessful()) {
+            List<Throwable> errors = resultFuture.getErrors();
+            throw errors.get(0);
+        }
 
-		// expect 1 result for each token
-		assertEquals(4, results.size());
+        // expect 1 result for each token
+        assertEquals(4, results.size());
 
-		for (AnalyzerResult analyzerResult : results) {
-			ValueDistributionResult vdResult = (ValueDistributionResult) analyzerResult;
-			ValueDistributionGroupResult result = vdResult.getSingleValueDistributionResult();
-			Collection<String> uniqueValues = new TreeSet<String>(result.getUniqueValues());
-			if ("first word".equals(vdResult.getColumnName())) {
-				assertEquals("ValueCountList[[[Sales->19], [VP->2]]]", result.getTopValues().toString());
-				assertTrue(result.getBottomValues().getValueCounts().isEmpty());
-				assertEquals(0, result.getNullCount());
-				assertEquals(2, result.getUniqueCount());
-			} else if ("second word".equals(vdResult.getColumnName())) {
-				assertEquals("ValueCountList[[[Rep->17], [Manager->3]]]", result.getTopValues().toString());
-				assertTrue(result.getBottomValues().getValueCounts().isEmpty());
-				assertEquals(1, result.getNullCount());
-				assertEquals(2, result.getUniqueCount());
-			} else if ("third words".equals(vdResult.getColumnName())) {
-				assertEquals("ValueCountList[[]]", result.getTopValues().toString());
-				assertTrue(result.getBottomValues().getValueCounts().isEmpty());
-				assertEquals(20, result.getNullCount());
+        for (AnalyzerResult analyzerResult : results) {
+            ValueDistributionResult vdResult = (ValueDistributionResult) analyzerResult;
+            ValueDistributionGroupResult result = vdResult.getSingleValueDistributionResult();
+            Collection<String> uniqueValues = new TreeSet<String>(result.getUniqueValues());
+            if ("first word".equals(vdResult.getColumnName())) {
+                assertEquals("ValueCountList[[[Sales->19], [VP->2]]]", result.getTopValues().toString());
+                assertTrue(result.getBottomValues().getValueCounts().isEmpty());
+                assertEquals(0, result.getNullCount());
+                assertEquals(2, result.getUniqueCount());
+            } else if ("second word".equals(vdResult.getColumnName())) {
+                assertEquals("ValueCountList[[[Rep->17], [Manager->3]]]", result.getTopValues().toString());
+                assertTrue(result.getBottomValues().getValueCounts().isEmpty());
+                assertEquals(1, result.getNullCount());
+                assertEquals(2, result.getUniqueCount());
+            } else if ("third words".equals(vdResult.getColumnName())) {
+                assertEquals("ValueCountList[[]]", result.getTopValues().toString());
+                assertTrue(result.getBottomValues().getValueCounts().isEmpty());
+                assertEquals(20, result.getNullCount());
 
-				assertEquals(3, result.getUniqueCount());
-				assertEquals("[(EMEA), (JAPAN,, (NA)]", uniqueValues.toString());
-			} else if ("fourth words".equals(vdResult.getColumnName())) {
-				assertEquals("ValueCountList[[]]", result.getTopValues().toString());
-				assertTrue(result.getBottomValues().getValueCounts().isEmpty());
-				assertEquals(22, result.getNullCount());
+                assertEquals(3, result.getUniqueCount());
+                assertEquals("[(EMEA), (JAPAN,, (NA)]", uniqueValues.toString());
+            } else if ("fourth words".equals(vdResult.getColumnName())) {
+                assertEquals("ValueCountList[[]]", result.getTopValues().toString());
+                assertTrue(result.getBottomValues().getValueCounts().isEmpty());
+                assertEquals(22, result.getNullCount());
 
-				assertEquals(1, result.getUniqueCount());
-				assertEquals("[APAC)]", uniqueValues.toString());
-			} else {
-				fail("Unexpected columnName: " + vdResult.getColumnName());
-			}
-		}
+                assertEquals(1, result.getUniqueCount());
+                assertEquals("[APAC)]", uniqueValues.toString());
+            } else {
+                fail("Unexpected columnName: " + vdResult.getColumnName());
+            }
+        }
 
-		con.close();
-		taskRunner.shutdown();
-	}
+        con.close();
+        taskRunner.shutdown();
+    }
 }
