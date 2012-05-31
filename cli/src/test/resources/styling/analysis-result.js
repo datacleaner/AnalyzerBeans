ae851cd0358f3ae7833460504477583c7da28c22
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-05-31 21:55:34 +0000

    improved stylesheet for HTML rendering and added a few more analyzer icons

--- cli/src/test/resources/styling/analysis-result.js
+++ cli/src/test/resources/styling/analysis-result.js
@@ -1,9 +1,14 @@
 var analysisResult = {};
 
 function drillToDetails(elementId) {
+	var wWidth = $(window).width();
+	var dWidth = wWidth * 0.85;
+	var wHeight = $(window).height();
+	var dHeight = wHeight * 0.8;
 	$('#' + elementId).dialog({
 		modal : true,
-		width : 700
+		width : dWidth,
+		height : dHeight
 	});
 }
 
