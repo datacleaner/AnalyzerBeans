e29833b39e9bb29f7b9fbb9f734b00869c6f9e2e
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-06-26 11:22:19 +0000

    Fixed issue in hashCode() for bean configurations (it only included keys!).

--- core/src/main/java/org/eobjects/analyzer/job/ImmutableBeanConfiguration.java
+++ core/src/main/java/org/eobjects/analyzer/job/ImmutableBeanConfiguration.java
@@ -28,73 +28,65 @@ import org.eobjects.analyzer.descriptors.ConfiguredPropertyDescriptor;
 import org.eobjects.analyzer.descriptors.PropertyDescriptor;
 import org.eobjects.metamodel.util.EqualsBuilder;
 
+/**
+ * Default (immutable) implementation of {@link BeanConfiguration}.
+ */
 public final class ImmutableBeanConfiguration implements BeanConfiguration {
 
-	private static final long serialVersionUID = 1L;
-	private final Map<PropertyDescriptor, Object> _properties;
+    private static final long serialVersionUID = 1L;
 
-	public ImmutableBeanConfiguration(Map<ConfiguredPropertyDescriptor, Object> properties) {
-		if (properties == null) {
-			_properties = new HashMap<PropertyDescriptor, Object>();
-		} else {
-			_properties = new HashMap<PropertyDescriptor, Object>(properties);
-		}
+    private final Map<PropertyDescriptor, Object> _properties;
 
-		// validate contents
-		for (Map.Entry<PropertyDescriptor, Object> entry : _properties.entrySet()) {
-			Object value = entry.getValue();
-			if (value instanceof Collection) {
-				throw new IllegalArgumentException(
-						"Collection values are not allowed in BeanConfigurations. Violating entry: " + entry.getKey()
-								+ " -> " + entry.getValue());
-			}
-		}
-	}
+    public ImmutableBeanConfiguration(Map<ConfiguredPropertyDescriptor, Object> properties) {
+        if (properties == null) {
+            _properties = new HashMap<PropertyDescriptor, Object>();
+        } else {
+            _properties = new HashMap<PropertyDescriptor, Object>(properties);
+        }
 
-	@Override
-	public Object getProperty(ConfiguredPropertyDescriptor propertyDescriptor) {
-		return _properties.get(propertyDescriptor);
-	}
+        // validate contents
+        for (Map.Entry<PropertyDescriptor, Object> entry : _properties.entrySet()) {
+            Object value = entry.getValue();
+            if (value instanceof Collection) {
+                throw new IllegalArgumentException(
+                        "Collection values are not allowed in BeanConfigurations. Violating entry: " + entry.getKey()
+                                + " -> " + entry.getValue());
+            }
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "ImmutableBeanConfiguration[" + _properties + "]";
-	}
+    @Override
+    public Object getProperty(ConfiguredPropertyDescriptor propertyDescriptor) {
+        return _properties.get(propertyDescriptor);
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((_properties == null) ? 0 : _properties.keySet().hashCode());
-		return result;
-	}
+    @Override
+    public String toString() {
+        return "ImmutableBeanConfiguration[" + _properties + "]";
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ImmutableBeanConfiguration other = (ImmutableBeanConfiguration) obj;
-		Set<PropertyDescriptor> keySet = _properties.keySet();
-		if (other._properties.keySet().equals(keySet)) {
-			for (PropertyDescriptor key : keySet) {
-				Object v1 = _properties.get(key);
-				Object v2 = other._properties.get(key);
-				if (v1 != v2 && !v1.equals(v2)) {
-					if (v1.getClass().isArray() && v2.getClass().isArray()) {
-						if (!EqualsBuilder.equals(v1, v2)) {
-							return false;
-						}
-					} else {
-						return false;
-					}
-				}
-			}
-			return true;
-		}
-		return false;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((_properties == null) ? 0 : _properties.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ImmutableBeanConfiguration other = (ImmutableBeanConfiguration) obj;
+        if (_properties == null) {
+            if (other._properties != null)
+                return false;
+        } else if (!_properties.equals(other._properties))
+            return false;
+        return true;
+    }
 }
