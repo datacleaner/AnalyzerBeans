019644ba44ea375c9f44671625af5436fb97cbe0
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-08-08 11:33:06 +0000

    specified query type (Query or String) in executeQuery to prepare for MetaModel 3.1.

--- components/basic-explorers/src/test/java/org/eobjects/analyzer/beans/ReferentialIntegrityValidatorTest.java
+++ components/basic-explorers/src/test/java/org/eobjects/analyzer/beans/ReferentialIntegrityValidatorTest.java
@@ -50,153 +50,155 @@ import org.eobjects.metamodel.schema.Table;
 
 public class ReferentialIntegrityValidatorTest extends TestCase {
 
-	private DatastoreConnection con;
-	private DataContext dc;
-	private Table employeesTable;
-	private Table officesTable;
+    private DatastoreConnection con;
+    private DataContext dc;
+    private Table employeesTable;
+    private Table officesTable;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		con = TestHelper.createSampleDatabaseDatastore("ds").openConnection();
-		dc = con.getDataContext();
-		employeesTable = dc.getDefaultSchema().getTableByName("EMPLOYEES");
-		officesTable = dc.getDefaultSchema().getTableByName("OFFICES");
-	}
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        con = TestHelper.createSampleDatabaseDatastore("ds").openConnection();
+        dc = con.getDataContext();
+        employeesTable = dc.getDefaultSchema().getTableByName("EMPLOYEES");
+        officesTable = dc.getDefaultSchema().getTableByName("OFFICES");
+    }
 
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		con.close();
-	}
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        con.close();
+    }
 
-	public void testDescriptor() throws Exception {
-		ExplorerBeanDescriptor<ReferentialIntegrityValidator> descriptor = Descriptors
-				.ofExplorer(ReferentialIntegrityValidator.class);
-		assertEquals("AnnotationBasedExplorerBeanDescriptor[org.eobjects.analyzer.beans.ReferentialIntegrityValidator]",
-				descriptor.toString());
+    public void testDescriptor() throws Exception {
+        ExplorerBeanDescriptor<ReferentialIntegrityValidator> descriptor = Descriptors
+                .ofExplorer(ReferentialIntegrityValidator.class);
+        assertEquals(
+                "AnnotationBasedExplorerBeanDescriptor[org.eobjects.analyzer.beans.ReferentialIntegrityValidator]",
+                descriptor.toString());
 
-		Set<ConfiguredPropertyDescriptor> configuredProperties = descriptor.getConfiguredProperties();
-		Iterator<ConfiguredPropertyDescriptor> it = configuredProperties.iterator();
-		assertTrue(it.hasNext());
-		assertEquals("Primary key column", it.next().getName());
-		assertTrue(it.hasNext());
-		assertEquals("Foreign key column", it.next().getName());
-		assertTrue(it.hasNext());
-		assertEquals("Accept NULL foreign keys?", it.next().getName());
-		assertFalse(it.hasNext());
+        Set<ConfiguredPropertyDescriptor> configuredProperties = descriptor.getConfiguredProperties();
+        Iterator<ConfiguredPropertyDescriptor> it = configuredProperties.iterator();
+        assertTrue(it.hasNext());
+        assertEquals("Primary key column", it.next().getName());
+        assertTrue(it.hasNext());
+        assertEquals("Foreign key column", it.next().getName());
+        assertTrue(it.hasNext());
+        assertEquals("Accept NULL foreign keys?", it.next().getName());
+        assertFalse(it.hasNext());
 
-		assertEquals("[]", descriptor.getProvidedProperties().toString());
-		
-		assertEquals("[]", descriptor.getValidateMethods().toString());
+        assertEquals("[]", descriptor.getProvidedProperties().toString());
 
-		assertEquals("[]", descriptor.getInitializeMethods().toString());
+        assertEquals("[]", descriptor.getValidateMethods().toString());
 
-		assertEquals("[]", descriptor.getCloseMethods().toString());
-	}
+        assertEquals("[]", descriptor.getInitializeMethods().toString());
 
-	public void testSeparateTables() throws Exception {
-		Column columnInOfficesTable = officesTable.getColumnByName("OFFICECODE");
-		Column columnInEmployeesTable = employeesTable.getColumnByName("OFFICECODE");
+        assertEquals("[]", descriptor.getCloseMethods().toString());
+    }
 
-		ReferentialIntegrityValidator bean = new ReferentialIntegrityValidator();
-		bean.setAcceptNullForeignKey(false);
-		bean.setPrimaryKeyColumn(columnInOfficesTable);
-		bean.setForeignKeyColumn(columnInEmployeesTable);
+    public void testSeparateTables() throws Exception {
+        Column columnInOfficesTable = officesTable.getColumnByName("OFFICECODE");
+        Column columnInEmployeesTable = employeesTable.getColumnByName("OFFICECODE");
 
-		bean.run(dc);
+        ReferentialIntegrityValidator bean = new ReferentialIntegrityValidator();
+        bean.setAcceptNullForeignKey(false);
+        bean.setPrimaryKeyColumn(columnInOfficesTable);
+        bean.setForeignKeyColumn(columnInEmployeesTable);
 
-		DataSetResult invalidRows = bean.getResult();
-		assertEquals(0, invalidRows.getRows().size());
-	}
+        bean.run(dc);
 
-	public void testReferentialInconsistency() throws Exception {
-		MutableSchema schema = new MutableSchema("projects");
+        DataSetResult invalidRows = bean.getResult();
+        assertEquals(0, invalidRows.getRows().size());
+    }
 
-		MutableTable roleTable = new MutableTable("role").setSchema(schema);
-		roleTable.addColumn(new MutableColumn("project_id").setTable(roleTable).setType(ColumnType.INTEGER));
-		roleTable.addColumn(new MutableColumn("name").setTable(roleTable).setType(ColumnType.INTEGER));
-		roleTable.addColumn(new MutableColumn("contributor_id").setTable(roleTable).setType(ColumnType.INTEGER));
-		schema.addTable(roleTable);
+    public void testReferentialInconsistency() throws Exception {
+        MutableSchema schema = new MutableSchema("projects");
 
-		MutableTable contributorTable = new MutableTable("contributor").setSchema(schema);
-		contributorTable.addColumn(new MutableColumn("contributor_id").setTable(contributorTable)
-				.setType(ColumnType.INTEGER));
-		schema.addTable(contributorTable);
+        MutableTable roleTable = new MutableTable("role").setSchema(schema);
+        roleTable.addColumn(new MutableColumn("project_id").setTable(roleTable).setType(ColumnType.INTEGER));
+        roleTable.addColumn(new MutableColumn("name").setTable(roleTable).setType(ColumnType.INTEGER));
+        roleTable.addColumn(new MutableColumn("contributor_id").setTable(roleTable).setType(ColumnType.INTEGER));
+        schema.addTable(roleTable);
 
-		Column foreignKeyColumn = schema.getTableByName("role").getColumnByName("contributor_id");
-		Column primaryKeyColumn = schema.getTableByName("contributor").getColumnByName("contributor_id");
+        MutableTable contributorTable = new MutableTable("contributor").setSchema(schema);
+        contributorTable.addColumn(new MutableColumn("contributor_id").setTable(contributorTable).setType(
+                ColumnType.INTEGER));
+        schema.addTable(contributorTable);
 
-		ReferentialIntegrityValidator bean = new ReferentialIntegrityValidator();
-		bean.setAcceptNullForeignKey(false);
-		bean.setPrimaryKeyColumn(primaryKeyColumn);
-		bean.setForeignKeyColumn(foreignKeyColumn);
+        Column foreignKeyColumn = schema.getTableByName("role").getColumnByName("contributor_id");
+        Column primaryKeyColumn = schema.getTableByName("contributor").getColumnByName("contributor_id");
 
-		DataContext dcMock = EasyMock.createMock(DataContext.class);
+        ReferentialIntegrityValidator bean = new ReferentialIntegrityValidator();
+        bean.setAcceptNullForeignKey(false);
+        bean.setPrimaryKeyColumn(primaryKeyColumn);
+        bean.setForeignKeyColumn(foreignKeyColumn);
 
-		List<Row> rows = new ArrayList<Row>();
+        DataContext dcMock = EasyMock.createMock(DataContext.class);
 
-		// Create SelectItems similar to those created in the bean (to make
-		// query-mocking work)
-		FromItem leftSide = new FromItem(new Query().select(foreignKeyColumn).from(foreignKeyColumn.getTable()))
-				.setAlias("a");
-		SelectItem leftOn = leftSide.getSubQuery().getSelectClause().getItem(0);
-		FromItem rightSide = new FromItem(new Query().select(primaryKeyColumn).from(primaryKeyColumn.getTable()))
-				.setAlias("b");
-		SelectItem rightOn = rightSide.getSubQuery().getSelectClause().getItem(0);
-		SelectItem primaryKeySelectItem = new SelectItem(rightOn, rightSide);
-		SelectItem foreignKeySelectItem = new SelectItem(leftOn, leftSide);
+        List<Row> rows = new ArrayList<Row>();
 
-		// An valid row
-		rows.add(new DefaultRow(new SelectItem[] { foreignKeySelectItem, primaryKeySelectItem },
-				new Object[] { "foo", "foo" }));
-		// An invalid row
-		rows.add(new DefaultRow(new SelectItem[] { foreignKeySelectItem, primaryKeySelectItem },
-				new Object[] { "bar", null }));
-		// Simulate an (impossible?) error in the join
-		rows.add(new DefaultRow(new SelectItem[] { foreignKeySelectItem, primaryKeySelectItem }, new Object[] { "foobar",
-				"foo" }));
-		// Another valid row
-		rows.add(new DefaultRow(new SelectItem[] { foreignKeySelectItem, primaryKeySelectItem },
-				new Object[] { "foo", "foo" }));
+        // Create SelectItems similar to those created in the bean (to make
+        // query-mocking work)
+        FromItem leftSide = new FromItem(new Query().select(foreignKeyColumn).from(foreignKeyColumn.getTable()))
+                .setAlias("a");
+        SelectItem leftOn = leftSide.getSubQuery().getSelectClause().getItem(0);
+        FromItem rightSide = new FromItem(new Query().select(primaryKeyColumn).from(primaryKeyColumn.getTable()))
+                .setAlias("b");
+        SelectItem rightOn = rightSide.getSubQuery().getSelectClause().getItem(0);
+        SelectItem primaryKeySelectItem = new SelectItem(rightOn, rightSide);
+        SelectItem foreignKeySelectItem = new SelectItem(leftOn, leftSide);
 
-		EasyMock.reportMatcher(new QueryMatcher("SELECT b.contributor_id, a.contributor_id, a.project_id, a.name "
-				+ "FROM (SELECT role.contributor_id, role.project_id, role.name FROM projects.role) a "
-				+ "LEFT JOIN (SELECT contributor.contributor_id FROM projects.contributor) b "
-				+ "ON a.contributor_id = b.contributor_id"));
-		EasyMock.expect(dcMock.executeQuery(null)).andReturn(new InMemoryDataSet(rows));
+        // An valid row
+        rows.add(new DefaultRow(new SelectItem[] { foreignKeySelectItem, primaryKeySelectItem }, new Object[] { "foo",
+                "foo" }));
+        // An invalid row
+        rows.add(new DefaultRow(new SelectItem[] { foreignKeySelectItem, primaryKeySelectItem }, new Object[] { "bar",
+                null }));
+        // Simulate an (impossible?) error in the join
+        rows.add(new DefaultRow(new SelectItem[] { foreignKeySelectItem, primaryKeySelectItem }, new Object[] {
+                "foobar", "foo" }));
+        // Another valid row
+        rows.add(new DefaultRow(new SelectItem[] { foreignKeySelectItem, primaryKeySelectItem }, new Object[] { "foo",
+                "foo" }));
 
-		EasyMock.replay(dcMock);
+        EasyMock.reportMatcher(new QueryMatcher("SELECT b.contributor_id, a.contributor_id, a.project_id, a.name "
+                + "FROM (SELECT role.contributor_id, role.project_id, role.name FROM projects.role) a "
+                + "LEFT JOIN (SELECT contributor.contributor_id FROM projects.contributor) b "
+                + "ON a.contributor_id = b.contributor_id"));
+        EasyMock.expect(dcMock.executeQuery((Query) null)).andReturn(new InMemoryDataSet(rows));
 
-		bean.run(dcMock);
+        EasyMock.replay(dcMock);
 
-		EasyMock.verify(dcMock);
+        bean.run(dcMock);
 
-		DataSetResult invalidRows = bean.getResult();
-		assertEquals(2, invalidRows.getRows().size());
+        EasyMock.verify(dcMock);
 
-		assertEquals("Row[values=[bar, null]]", invalidRows.getRows().get(0).toString());
-		assertEquals("Row[values=[foobar, foo]]", invalidRows.getRows().get(1).toString());
-	}
+        DataSetResult invalidRows = bean.getResult();
+        assertEquals(2, invalidRows.getRows().size());
 
-	public void testParentChildRelationship() throws Exception {
-		ReferentialIntegrityValidator bean = new ReferentialIntegrityValidator();
-		bean.setAcceptNullForeignKey(false);
-		bean.setPrimaryKeyColumn(employeesTable.getColumnByName("EMPLOYEENUMBER"));
-		bean.setForeignKeyColumn(employeesTable.getColumnByName("REPORTSTO"));
+        assertEquals("Row[values=[bar, null]]", invalidRows.getRows().get(0).toString());
+        assertEquals("Row[values=[foobar, foo]]", invalidRows.getRows().get(1).toString());
+    }
 
-		bean.run(dc);
+    public void testParentChildRelationship() throws Exception {
+        ReferentialIntegrityValidator bean = new ReferentialIntegrityValidator();
+        bean.setAcceptNullForeignKey(false);
+        bean.setPrimaryKeyColumn(employeesTable.getColumnByName("EMPLOYEENUMBER"));
+        bean.setForeignKeyColumn(employeesTable.getColumnByName("REPORTSTO"));
 
-		DataSetResult invalidRows = bean.getResult();
-		assertEquals(1, invalidRows.getRows().size());
-		assertEquals("Row[values=[null, null, 1002, Murphy, Diane, x5800, dmurphy@classicmodelcars.com, 1, President]]",
-				invalidRows.getRows().get(0).toString());
+        bean.run(dc);
 
-		bean.setAcceptNullForeignKey(true);
+        DataSetResult invalidRows = bean.getResult();
+        assertEquals(1, invalidRows.getRows().size());
+        assertEquals(
+                "Row[values=[null, null, 1002, Murphy, Diane, x5800, dmurphy@classicmodelcars.com, 1, President]]",
+                invalidRows.getRows().get(0).toString());
 
-		bean.run(dc);
+        bean.setAcceptNullForeignKey(true);
 
-		invalidRows = bean.getResult();
-		assertEquals(0, invalidRows.getRows().size());
-	}
+        bean.run(dc);
+
+        invalidRows = bean.getResult();
+        assertEquals(0, invalidRows.getRows().size());
+    }
 }
