48b31b6834b8a9bc8ffd641c7f225491376f7180
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2012-10-19 12:26:07 +0000

    Improved usage awareness with atomic constructs instead of volatile.

--- core/src/main/java/org/eobjects/analyzer/connection/UsageAwareDatastoreConnection.java
+++ core/src/main/java/org/eobjects/analyzer/connection/UsageAwareDatastoreConnection.java
@@ -19,6 +19,9 @@
  */
 package org.eobjects.analyzer.connection;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.eobjects.metamodel.DataContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,110 +36,119 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class UsageAwareDatastoreConnection<E extends DataContext> implements DatastoreConnection {
 
-	private static final Logger logger = LoggerFactory.getLogger(UsageAwareDatastoreConnection.class);
+    private static final Logger logger = LoggerFactory.getLogger(UsageAwareDatastoreConnection.class);
 
-	private final Datastore _datastore;
-	private volatile int _usageCount;
+    private final Datastore _datastore;
+    private final AtomicInteger _usageCount;
+    private final AtomicBoolean _closed;
 
-	public UsageAwareDatastoreConnection(Datastore datastore) {
-		_datastore = datastore;
-		_usageCount = 1;
-		if (logger.isDebugEnabled()) {
-			StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-			logger.debug("{} instantiated by:", this);
-			for (int i = 0; i < stackTrace.length && i < 7; i++) {
-				StackTraceElement ste = stackTrace[i];
-				logger.debug(" - {} @ line {}", ste.getClassName(), ste.getLineNumber());
-			}
-		}
-	}
+    public UsageAwareDatastoreConnection(Datastore datastore) {
+        _datastore = datastore;
+        _usageCount = new AtomicInteger(1);
+        _closed = new AtomicBoolean(false);
+        if (logger.isDebugEnabled()) {
+            StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+            logger.debug("{} instantiated by:", this);
+            for (int i = 0; i < stackTrace.length && i < 7; i++) {
+                StackTraceElement ste = stackTrace[i];
+                logger.debug(" - {} @ line {}", ste.getClassName(), ste.getLineNumber());
+            }
+        }
+    }
 
-	public synchronized final boolean requestUsage() {
-		if (isClosed()) {
-			logger.debug("Connection is closed, request for more usage refused");
-			return false;
-		}
+    public synchronized final boolean requestUsage() {
+        if (isClosed()) {
+            return false;
+        }
 
-		_usageCount++;
+        final int usage = _usageCount.incrementAndGet();
+        if (usage == 1) {
+            _closed.set(true);
+            _usageCount.decrementAndGet();
+            logger.debug("Connection is closed, request for more usage refused");
+            return false;
+        }
 
-		logger.debug("Usage incremented to {} for {}", _usageCount, this);
+        logger.debug("Usage incremented to {} for {}", usage, this);
 
-		if (logger.isDebugEnabled()) {
-			StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-			logger.debug("Incremented usage by:");
-			for (int i = 0; i < stackTrace.length && i < 7; i++) {
-				StackTraceElement ste = stackTrace[i];
-				logger.debug(" - {} @ line {}", ste.getClassName(), ste.getLineNumber());
-			}
-		}
-		return true;
-	}
+        if (logger.isDebugEnabled()) {
+            StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+            logger.debug("Incremented usage by:");
+            for (int i = 0; i < stackTrace.length && i < 7; i++) {
+                StackTraceElement ste = stackTrace[i];
+                logger.debug(" - {} @ line {}", ste.getClassName(), ste.getLineNumber());
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public abstract E getDataContext();
+    @Override
+    public abstract E getDataContext();
 
-	public synchronized boolean isClosed() {
-		return _usageCount == 0;
-	}
+    public synchronized boolean isClosed() {
+        return _closed.get();
+    }
 
-	@Override
-	public synchronized final void close() {
-		if (_usageCount == 0) {
-			logger.warn("Connection is already closed, but close() was invoked!", new Throwable());
-			return;
-		}
+    @Override
+    public synchronized final void close() {
+        if (isClosed()) {
+            logger.warn("Connection is already closed, but close() was invoked!", new Throwable());
+            return;
+        }
 
-		_usageCount--;
-		logger.debug("Method close() invoked, usage decremented to {} for {}", _usageCount, this);
-		if (_usageCount == 0) {
-			logger.debug("Closing {}", this);
-			closeInternal();
-		}
-	}
+        final int usage = _usageCount.decrementAndGet();
 
-	/**
-	 * Subclasses should implement this method to do the actual closing logic of
-	 * the datacontext
-	 */
-	protected abstract void closeInternal();
+        logger.debug("Method close() invoked, usage decremented to {} for {}", _usageCount, this);
+        if (usage == 0) {
+            _closed.set(true);
+            logger.debug("Closing {}", this);
+            closeInternal();
+        }
+    }
 
-	@Override
-	protected void finalize() throws Throwable {
-		super.finalize();
-		if (!isClosed()) {
-			if (logger.isWarnEnabled()) {
-				logger.warn(
-						"Method finalize() invoked but not all usages closed ({} remaining) (for {}). Closing DatastoreConnection.",
-						_usageCount, this);
-			}
-			// in case of gc, also do the closing
-			closeInternal();
-		}
-	}
+    /**
+     * Subclasses should implement this method to do the actual closing logic of
+     * the datacontext
+     */
+    protected abstract void closeInternal();
 
-	@Override
-	public String toString() {
-		return "UsageAwareDatastoreConnection[datastore=" + getDatastoreName() + ",usage=" + getUsageCount() + "]";
-	}
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        if (!isClosed()) {
+            if (logger.isWarnEnabled()) {
+                logger.warn(
+                        "Method finalize() invoked but not all usages closed ({} remaining) (for {}). Closing DatastoreConnection.",
+                        _usageCount, this);
+            }
+            // in case of gc, also do the closing
+            closeInternal();
+        }
+    }
 
-	@Override
-	public final Datastore getDatastore() {
-		return _datastore;
-	}
+    @Override
+    public String toString() {
+        return "UsageAwareDatastoreConnection[datastore=" + getDatastoreName() + ",usage=" + getUsageCount() + "]";
+    }
 
-	private String getDatastoreName() {
-		if (_datastore != null) {
-			return _datastore.getName();
-		}
-		return "<null>";
-	}
+    @Override
+    public final Datastore getDatastore() {
+        return _datastore;
+    }
 
-	/**
-	 * Gets the amount of usages this datacontext provider currently has.
-	 * 
-	 * @return
-	 */
-	public int getUsageCount() {
-		return _usageCount;
-	}
+    private String getDatastoreName() {
+        if (_datastore != null) {
+            return _datastore.getName();
+        }
+        return "<null>";
+    }
+
+    /**
+     * Gets the amount of usages this datacontext provider currently has.
+     * 
+     * @return
+     */
+    public int getUsageCount() {
+        return _usageCount.get();
+    }
 }
