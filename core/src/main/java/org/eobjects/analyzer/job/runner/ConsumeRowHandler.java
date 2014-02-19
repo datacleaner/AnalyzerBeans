7fcedbff77a3df9ff0c72c8b1d19dacb50d97ae4
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2014-02-19 14:37:47 +0000

    Ticket #1213: Added ConsumeRowHandler object which can handle the consumers of a job on a record-by-record level.

---
+++ core/src/main/java/org/eobjects/analyzer/job/runner/ConsumeRowHandler.java
@@ -0,0 +1,158 @@
+/**
+ * eobjects.org AnalyzerBeans
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.analyzer.job.runner;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.eobjects.analyzer.beans.api.Analyzer;
+import org.eobjects.analyzer.configuration.AnalyzerBeansConfiguration;
+import org.eobjects.analyzer.configuration.InjectionManager;
+import org.eobjects.analyzer.data.InputRow;
+import org.eobjects.analyzer.job.AnalysisJob;
+import org.eobjects.analyzer.job.concurrent.SingleThreadedTaskRunner;
+import org.eobjects.analyzer.job.concurrent.TaskListener;
+import org.eobjects.analyzer.job.tasks.Task;
+import org.eobjects.analyzer.lifecycle.LifeCycleHelper;
+import org.eobjects.analyzer.util.SourceColumnFinder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Object that can handle the task of consuming rows.
+ * 
+ * TODO: Improve doc
+ */
+public class ConsumeRowHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConsumeRowHandler.class);
+
+    private final List<RowProcessingConsumer> _consumers;
+
+    public static class Configuration {
+        public boolean includeNonDistributedTasks = true;
+        public AnalysisListener analysisListener = new InfoLoggingAnalysisListener();
+        public boolean includeAnalyzers = true;
+    }
+
+    public ConsumeRowHandler(AnalysisJob job, AnalyzerBeansConfiguration analyzerBeansConfiguration,
+            Configuration configuration) {
+        // TODO: Verify that there is only one source table
+        _consumers = extractConsumers(job, analyzerBeansConfiguration, configuration);
+    }
+
+    public ConsumeRowHandler(List<RowProcessingConsumer> consumers) {
+        _consumers = consumers;
+    }
+
+    /**
+     * Gets the {@link RowProcessingConsumer}s that this handler is working on.
+     * 
+     * @return
+     */
+    public List<RowProcessingConsumer> getConsumers() {
+        return _consumers;
+    }
+
+    public List<InputRow> consume(InputRow row) {
+        OutcomeSink outcomes = new OutcomeSinkImpl();
+        ConsumeRowHandlerDelegate delegate = new ConsumeRowHandlerDelegate(_consumers, row, 0, outcomes);
+        List<InputRow> result = delegate.consume();
+        return result;
+    }
+
+    private List<RowProcessingConsumer> extractConsumers(AnalysisJob analysisJob,
+            AnalyzerBeansConfiguration analyzerBeansConfiguration, Configuration configuration) {
+        final InjectionManager injectionManager = analyzerBeansConfiguration.getInjectionManager(analysisJob);
+        final ReferenceDataActivationManager referenceDataActivationManager = new ReferenceDataActivationManager();
+
+        final LifeCycleHelper lifeCycleHelper = new LifeCycleHelper(injectionManager, referenceDataActivationManager,
+                configuration.includeNonDistributedTasks);
+        SourceColumnFinder sourceColumnFinder = new SourceColumnFinder();
+        sourceColumnFinder.addSources(analysisJob);
+
+        /**
+         * Use a single threaded task runner since this handler is invoked in a
+         * blocking way - the calling code may itself be multithreaded without
+         * issues.
+         */
+        final SingleThreadedTaskRunner taskRunner = new SingleThreadedTaskRunner();
+
+        final AnalysisListener analysisListener = configuration.analysisListener;
+        final RowProcessingPublishers rowProcessingPublishers = new RowProcessingPublishers(analysisJob,
+                analysisListener, taskRunner, lifeCycleHelper, sourceColumnFinder);
+
+        final Collection<RowProcessingPublisher> publisherCollection = rowProcessingPublishers
+                .getRowProcessingPublishers();
+
+        // TODO: Throw if not correct
+        assert publisherCollection.size() == 1;
+
+        final RowProcessingPublisher publisher = publisherCollection.iterator().next();
+
+        final AtomicReference<Throwable> errorReference = new AtomicReference<Throwable>();
+
+        publisher.initializeConsumers(new TaskListener() {
+            @Override
+            public void onError(Task task, Throwable throwable) {
+                logger.error("Exception thrown while initializing consumers.", throwable);
+                errorReference.compareAndSet(null, throwable);
+            }
+
+            @Override
+            public void onComplete(Task task) {
+                logger.info("Consumers initialized successfully.");
+            }
+
+            @Override
+            public void onBegin(Task task) {
+                logger.info("Beginning the process of initializing consumers.");
+            }
+        });
+
+        final Throwable throwable = errorReference.get();
+        if (throwable != null) {
+            if (throwable instanceof RuntimeException) {
+
+            }
+        }
+
+        List<RowProcessingConsumer> consumers = publisher.getConfigurableConsumers();
+        if (!configuration.includeAnalyzers) {
+            consumers = removeAnalyzers(consumers);
+        }
+
+        consumers = RowProcessingPublisher.sortConsumers(consumers);
+        return consumers;
+    }
+
+    private List<RowProcessingConsumer> removeAnalyzers(List<RowProcessingConsumer> consumers) {
+        final List<RowProcessingConsumer> result = new ArrayList<RowProcessingConsumer>();
+        for (RowProcessingConsumer rowProcessingConsumer : consumers) {
+            final Object component = rowProcessingConsumer.getComponent();
+            if (!(component instanceof Analyzer<?>)) {
+                result.add(rowProcessingConsumer);
+            }
+        }
+        return result;
+    }
+}
---
+++ core/src/main/java/org/eobjects/analyzer/job/runner/ConsumeRowHandlerDelegate.java
@@ -0,0 +1,82 @@
+/**
+ * eobjects.org AnalyzerBeans
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.analyzer.job.runner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eobjects.analyzer.data.InputRow;
+
+/**
+ * Delegate execution object for {@link ConsumeRowHandler}. Contains state
+ * particular to processing of a single consumer in the chain, and also handles
+ * recursive logic coming from {@link RowProcessingChain} callbacks.
+ */
+final class ConsumeRowHandlerDelegate implements RowProcessingChain {
+
+    private final List<RowProcessingConsumer> _consumers;
+    private final InputRow _row;
+    private final int _consumerIndex;
+    private final OutcomeSink _outcomes;
+    private final List<InputRow> _resultRecords;
+
+    public ConsumeRowHandlerDelegate(List<RowProcessingConsumer> consumers, InputRow row, int consumerIndex,
+            OutcomeSink outcomes) {
+        this(consumers, row, consumerIndex, outcomes, new ArrayList<InputRow>(1));
+    }
+
+    private ConsumeRowHandlerDelegate(List<RowProcessingConsumer> consumers, InputRow row, int consumerIndex,
+            OutcomeSink outcomes, List<InputRow> resultRecords) {
+        _consumers = consumers;
+        _row = row;
+        _consumerIndex = consumerIndex;
+        _outcomes = outcomes;
+        _resultRecords = resultRecords;
+    }
+
+    public List<InputRow> consume() {
+        RowProcessingConsumer consumer = _consumers.get(_consumerIndex);
+
+        final boolean process = consumer.satisfiedForConsume(_outcomes.getOutcomes(), _row);
+        if (process) {
+            consumer.consume(_row, 1, _outcomes, this);
+        } else {
+            // jump to the next step
+            processNext(_row, 1, _outcomes);
+        }
+
+        return _resultRecords;
+    }
+
+    @Override
+    public void processNext(InputRow row, int distinctCount, OutcomeSink outcomes) {
+        final int nextIndex = _consumerIndex + 1;
+        if (nextIndex >= _consumers.size()) {
+            // finished!
+            _resultRecords.add(row);
+            return;
+        }
+
+        final ConsumeRowHandlerDelegate subDelegate = new ConsumeRowHandlerDelegate(_consumers, row, nextIndex,
+                outcomes, _resultRecords);
+        subDelegate.consume();
+    }
+
+}
