85cda59900d215afcf933ed0945d8c2aa647da9a
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2014-04-25 14:34:00 +0200

    Issue #20: Added conversion from CLOB->String and BLOB->byte[]

--- core/src/main/java/org/eobjects/analyzer/data/MetaModelInputColumn.java
+++ core/src/main/java/org/eobjects/analyzer/data/MetaModelInputColumn.java
@@ -24,82 +24,86 @@ import org.eobjects.metamodel.schema.Column;
 import org.eobjects.metamodel.schema.ColumnType;
 
 public final class MetaModelInputColumn extends AbstractInputColumn<Object> {
-	
-	private static final long serialVersionUID = 1L;
 
-	private final Column _column;
+    private static final long serialVersionUID = 1L;
 
-	public MetaModelInputColumn(Column column) {
-		if (column == null) {
-			throw new IllegalArgumentException("column cannot be null");
-		}
-		_column = column;
-	}
+    private final Column _column;
 
-	@SuppressWarnings("unchecked")
-	public <E> InputColumn<E> narrow(Class<E> e) {
-		Class<?> javaEquivalentClass = _column.getType().getJavaEquivalentClass();
-		if (ReflectionUtils.is(javaEquivalentClass, e)) {
-			return (InputColumn<E>) this;
-		}
-		throw new IllegalArgumentException("Can only narrow this column to supertypes of: " + javaEquivalentClass);
-	}
+    public MetaModelInputColumn(Column column) {
+        if (column == null) {
+            throw new IllegalArgumentException("column cannot be null");
+        }
+        _column = column;
+    }
 
-	@Override
-	public String getName() {
-		return _column.getName();
-	}
+    @SuppressWarnings("unchecked")
+    public <E> InputColumn<E> narrow(Class<E> e) {
+        Class<?> javaEquivalentClass = _column.getType().getJavaEquivalentClass();
+        if (ReflectionUtils.is(javaEquivalentClass, e)) {
+            return (InputColumn<E>) this;
+        }
+        throw new IllegalArgumentException("Can only narrow this column to supertypes of: " + javaEquivalentClass);
+    }
 
-	@Override
-	protected Column getPhysicalColumnInternal() {
-		return _column;
-	}
+    @Override
+    public String getName() {
+        return _column.getName();
+    }
 
-	@Override
-	protected boolean equalsInternal(AbstractInputColumn<?> that) {
-		MetaModelInputColumn that2 = (MetaModelInputColumn) that;
-		return _column.equals(that2._column);
-	}
+    @Override
+    protected Column getPhysicalColumnInternal() {
+        return _column;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public Class<? extends Object> getDataType() {
-		ColumnType type = _column.getType();
-		if (type == null) {
-			return null;
-		}
-		return (Class<Object>) type.getJavaEquivalentClass();
-	}
+    @Override
+    protected boolean equalsInternal(AbstractInputColumn<?> that) {
+        MetaModelInputColumn that2 = (MetaModelInputColumn) that;
+        return _column.equals(that2._column);
+    }
 
-	@Override
-	protected int hashCodeInternal() {
-		return _column.hashCode();
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public Class<? extends Object> getDataType() {
+        ColumnType type = _column.getType();
+        if (type == null) {
+            return null;
+        } else if (type == ColumnType.CLOB) {
+            return String.class;
+        } else if (type == ColumnType.BLOB) {
+            return byte[].class;
+        }
+        return (Class<Object>) type.getJavaEquivalentClass();
+    }
 
-	@Override
-	@SuppressWarnings("deprecation")
-	public DataTypeFamily getDataTypeFamily() {
-		ColumnType type = _column.getType();
-		if (type == null) {
-			return DataTypeFamily.UNDEFINED;
-		}
-		if (type.isBoolean()) {
-			return DataTypeFamily.BOOLEAN;
-		}
-		if (type.isLiteral()) {
-			return DataTypeFamily.STRING;
-		}
-		if (type.isNumber()) {
-			return DataTypeFamily.NUMBER;
-		}
-		if (type.isTimeBased()) {
-			return DataTypeFamily.DATE;
-		}
-		return DataTypeFamily.UNDEFINED;
-	}
+    @Override
+    protected int hashCodeInternal() {
+        return _column.hashCode();
+    }
 
-	@Override
-	public String toString() {
-		return "MetaModelInputColumn[" + _column.getQualifiedLabel() + "]";
-	}
+    @Override
+    @SuppressWarnings("deprecation")
+    public DataTypeFamily getDataTypeFamily() {
+        ColumnType type = _column.getType();
+        if (type == null) {
+            return DataTypeFamily.UNDEFINED;
+        }
+        if (type.isBoolean()) {
+            return DataTypeFamily.BOOLEAN;
+        }
+        if (type.isLiteral()) {
+            return DataTypeFamily.STRING;
+        }
+        if (type.isNumber()) {
+            return DataTypeFamily.NUMBER;
+        }
+        if (type.isTimeBased()) {
+            return DataTypeFamily.DATE;
+        }
+        return DataTypeFamily.UNDEFINED;
+    }
+
+    @Override
+    public String toString() {
+        return "MetaModelInputColumn[" + _column.getQualifiedLabel() + "]";
+    }
 }
