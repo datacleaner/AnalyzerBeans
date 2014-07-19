64b5238a4079658252aa803f9b63126c2e1319e1
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2014-07-19 22:19:11 +0200

    Removed unused import

--- core/src/main/java/org/eobjects/analyzer/result/AbstractAnalysisResult.java
+++ core/src/main/java/org/eobjects/analyzer/result/AbstractAnalysisResult.java
@@ -21,7 +21,6 @@ package org.eobjects.analyzer.result;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
