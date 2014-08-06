c671e2ef2bf479218da74a13104ee1bce23c5368
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2014-08-06 15:16:42 +0200

    Added unittest for asserting equality of different type filteroutcomes

---
+++ core/src/test/java/org/eobjects/analyzer/job/AbstractFilterOutcomeTest.java
@@ -0,0 +1,67 @@
+/**
+ * eobjects.org AnalyzerBeans
+ * Copyright (C) 2010 eobjects.org
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.eobjects.analyzer.job;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.eobjects.analyzer.configuration.AnalyzerBeansConfigurationImpl;
+import org.eobjects.analyzer.data.MockInputColumn;
+import org.eobjects.analyzer.job.builder.AnalysisJobBuilder;
+import org.eobjects.analyzer.job.builder.FilterJobBuilder;
+import org.eobjects.analyzer.job.builder.LazyFilterOutcome;
+import org.eobjects.analyzer.test.MockFilter;
+import org.eobjects.analyzer.test.MockFilter.Category;
+
+import junit.framework.TestCase;
+
+public class AbstractFilterOutcomeTest extends TestCase {
+
+    public void testEqualsAndHashCodeOnDifferentSubclasses() throws Exception {
+        FilterOutcome fo1;
+        FilterOutcome fo2;
+
+        try (final AnalysisJobBuilder ajb = new AnalysisJobBuilder(new AnalyzerBeansConfigurationImpl())) {
+            FilterJobBuilder<MockFilter, Category> filterJobBuilder = ajb.addFilter(MockFilter.class);
+            filterJobBuilder.addInputColumn(new MockInputColumn<String>("foo"));
+            filterJobBuilder.getConfigurableBean().setSomeEnum(Category.INVALID);
+            filterJobBuilder.getConfigurableBean().setSomeFile(new File("."));
+
+            fo1 = filterJobBuilder.getFilterOutcome(Category.VALID);
+            assertTrue(fo1 instanceof LazyFilterOutcome);
+
+            final FilterJob filterJob = filterJobBuilder.toFilterJob();
+            
+            Collection<FilterOutcome> filterOutcomes = filterJob.getFilterOutcomes();
+            fo2 = null;
+            for (FilterOutcome filterOutcome : filterOutcomes) {
+                if (filterOutcome.getCategory() == Category.VALID) {
+                    fo2 = filterOutcome;
+                }
+            }
+
+            assertNotNull(fo2);
+            assertTrue(fo2 instanceof ImmutableFilterOutcome);
+        }
+        
+        assertTrue(fo1.equals(fo2));
+        assertEquals(fo1.hashCode(), fo2.hashCode());
+    }
+}
